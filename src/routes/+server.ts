d2da8fccb38063c6813ef6118fa98d992225ef22
Reece Harris <reece@codegalaxy.co.uk>
2025-04-17 10:55:52 +0100

    remove unused files and components; update random number generation to use a constant key

--- src/routes/+server.ts
+++ src/routes/+server.ts
@@ -1,17 +1,7 @@
 import { error } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
+import { key } from '$lib/constants';
 
-export const GET: RequestHandler = ({ url }) => {
-	const min = Number(url.searchParams.get('min') ?? '0');
-	const max = Number(url.searchParams.get('max') ?? '1');
-
-	const d = max - min;
-
-	if (isNaN(d) || d < 0) {
-		error(400, 'min and max must be numbers, and min must be less than max');
-	}
-
-	const random = min + Math.random() * d;
-
-	return new Response(String(random));
+export const GET: RequestHandler = () => {
+	return new Response(String(key.toString('hex')));
 };
